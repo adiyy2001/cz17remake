a1e3642655130261beae1d0c6dd8ca5e82113c71
root <root@DESKTOP-OF8DG1J.localdomain>
2020-02-24 15:48:13 +0100

    add landing page for products

--- src/app/product-managment/product-managment.module.ts
+++ src/app/product-managment/product-managment.module.ts
@@ -4,10 +4,13 @@ import { CommonModule } from '@angular/common';
 import { ProductManagmentRoutingModule } from './product-managment-routing.module';
 import { ProductManagmentComponent } from './product-managment.component';
 import { ListOfProductsComponent } from './list-of-products/list-of-products.component';
+import { FilterComponentComponent } from './filter-component/filter-component.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { TableFotterComponent } from './table-fotter/table-fotter.component';
 
 
 @NgModule({
-  declarations: [ProductManagmentComponent, ListOfProductsComponent],
+  declarations: [ProductManagmentComponent, ListOfProductsComponent, FilterComponentComponent, ProductListComponent, TableFotterComponent],
   imports: [
     CommonModule,
     ProductManagmentRoutingModule
---
+++ src/app/product-managment/table-fotter/table-fotter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-table-fotter',
+  templateUrl: './table-fotter.component.html',
+  styleUrls: ['./table-fotter.component.scss']
+})
+export class TableFotterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
